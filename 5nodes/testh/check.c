83c07dd08ced98d6f3df1d9a023945cd460ed205
umatomba <umatomba@gmail.com>
2015-02-03 13:03:18 +0100

    cleaning

--- 5nodes/testh/check.c
+++ 5nodes/testh/check.c
@@ -12,6 +12,7 @@
       enum hyperdex_client_returncode loop_status;
       size_t i;
       size_t k;
+      size_t j = 1000000;
       client = hyperdex_client_create("192.168.100.1", 1982);
       char name[15];
       char firstname[15];
@@ -20,28 +21,23 @@
 
       printf("check start\n");
 
-      for (k = 0; k < 1000000; ++k) {
+      for (k = 0; k < j; ++k) {
    	  sprintf(name, "username%d", k);
           sprintf(firstname, "John%d", k);
-	  
 	  op_id = hyperdex_client_get(client, "usertable", name, strlen(name), &op_status, &attrs, &attrs_sz);
           loop_id = hyperdex_client_loop(client, -1, &loop_status);
-	  
-	  //printf("gaot attribute \"%s\" = \"%.*s\"\n", attrs[0].attr, attrs[0].value_sz, attrs[0].value);
 	  sprintf(firstname2, "%.*s", attrs[0].value_sz, attrs[0].value);
-          //printf("got firstname2: %s\n", firstname2);
-          //printf("firstname: %s\n", firstname);
-	  
           ret = strncmp(firstname, firstname2);
 	
 	  if (ret != 0){
-                printf("last from DB: %s\n", firstname2);
-                printf("try get: %s\n", firstname);		 
+                printf("error on: %d\n", k);
+                printf("in DB value: %s\n", firstname2);
+                printf("must be: %s\n", firstname);		 
 		return 0; 
           }
       }
 
-      printf("check done\n");
+      printf("check %d records done\n", j);
 
       hyperdex_client_destroy_attrs(attrs, attrs_sz);
       hyperdex_client_destroy(client);
